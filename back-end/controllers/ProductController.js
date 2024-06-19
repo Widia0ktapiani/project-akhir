899502b4daaee82ae45efd3421cffa70e71a3f1b
widia oktapiani <oktapianiwidia30@gmail.com>
2024-06-19 22:44:28 +0700

    finish final project

--- back-end/controllers/ProductController.js
+++ back-end/controllers/ProductController.js
@@ -103,25 +103,6 @@ class ProductController {
         res.status(500).json({ error: error.message });
       }
     }
-    
-    
-  //   const { id } = req.params;
-  //   const { name, qty, categoryId, imageUrl, updatedBy } = req.body;
-  //   try {
-  //     const [updated] = await Products.update(
-  //       { name, qty, categoryId, imageUrl, updatedBy },
-  //       { where: { id } }
-  //     );
-  //     if (!updated) {
-  //       return res.status(404).json({ error: 'Product not found' });
-  //     }
-  //     const updatedProduct = await Products.findByPk(id);
-  //     res.json(updatedProduct);
-  //   } catch (error) {
-  //     res.status(500).json({ error: error.message });
-  //   }
-  // }
-
   // API untuk menghapus 1 produk
   static async deleteProduct(req, res) {
     const { id } = req.params;
